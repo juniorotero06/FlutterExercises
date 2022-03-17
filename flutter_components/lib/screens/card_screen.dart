15f2d65760d499cc05292e8f1f483867567dd76e
juniorotero06 <dgrotero697@gmail.com>
2022-03-17 14:19:44 -0500

    cards

--- flutter_components/lib/screens/card_screen.dart
+++ flutter_components/lib/screens/card_screen.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/widgets/widgets.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text("Card Widget")),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: const [
+            CustomCardType1(),
+            SizedBox(
+              height: 10,
+            ),
+            CustomCardType2(
+              imageUrl:
+                  "https://mymodernmet.com/wp/wp-content/uploads/2020/11/International-Landscape-Photographer-Year-PhotographER-1st-KelvinYuen-2.jpg",
+              textImg: "Un hermoso paisaje",
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomCardType2(
+              imageUrl:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlKXVC66ha52Wtp_wt098suCEqsPPOiQcl2NWtFPOEyt4a1l5wLLik16iaqTrDN5ipUYQ&usqp=CAU",
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomCardType2(
+              imageUrl:
+                  "https://predios.com.co/wp-content/uploads/2021/06/travel-landscape-01.jpg",
+              textImg: "Espectaculares sitios",
+            ),
+            SizedBox(
+              height: 100,
+            ),
+          ],
+        ));
   }
 }
--- flutter_components/lib/themes/app_theme.dart
+++ flutter_components/lib/themes/app_theme.dart
@@ -7,7 +7,9 @@ class AppTheme {
       primaryColor: primary,
 
       //Appbaar theme
-      appBarTheme: const AppBarTheme(color: primary, elevation: 0));
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
       //Color primario
