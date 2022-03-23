3d4795e8b7754d2c8b12e3f94b3226610df2c411
juniorotero06 <dgrotero697@gmail.com>
2022-03-23 16:22:36 -0500

    sliders y slives

--- peliculas_app/lib/screens/details_screen.dart
+++ peliculas_app/lib/screens/details_screen.dart
@@ -1,15 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_app/themes/app_theme.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? "no-movie";
+
     return Scaffold(
-        body: Container(
-      child: const Center(
-        child: Text("Details Screen"),
-      ),
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          _PosterAndTitle(),
+          _Overview(),
+          _Overview(),
+          _Overview(),
+          CastingCards()
+        ]))
+      ],
     ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: AppTheme.primary,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            color: Colors.black26,
+            padding: const EdgeInsets.only(bottom: 10),
+            child: const Text(
+              "movie.title",
+              style: TextStyle(fontSize: 16),
+            )),
+        background: const FadeInImage(
+          placeholder: AssetImage("assets/loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/500x300"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage("assets/no-image.jpg"),
+              image: NetworkImage("https://via.placeholder.com/300x400"),
+              height: 150,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "movie.title",
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                "movie.originalTitle",
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.star_outline,
+                    color: Colors.yellow,
+                    size: 15,
+                  ),
+                  const SizedBox(width: 5),
+                  Text("movie.voteAverage", style: textTheme.caption)
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        "Sint proident culpa aliqua aliqua laboris. Enim nulla eiusmod dolor cillum officia. Eu minim ullamco nisi nostrud consequat anim laborum ex sunt magna incididunt occaecat velit tempor. Mollit enim cupidatat ullamco voluptate nisi irure occaecat cupidatat exercitation. Aute ut nostrud velit voluptate anim. Et in cillum cillum ad reprehenderit anim do. Laboris anim ex nostrud id in.",
+        textAlign: TextAlign.justify,
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
+    );
   }
 }
