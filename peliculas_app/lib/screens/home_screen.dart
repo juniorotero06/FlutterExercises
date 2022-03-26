8424458df03dbe7e8cca372d2079d90659381227
juniorotero06 <dgrotero697@gmail.com>
2022-03-26 10:12:43 -0500

    infinite scroll populuar movies

--- peliculas_app/lib/screens/home_screen.dart
+++ peliculas_app/lib/screens/home_screen.dart
@@ -25,7 +25,10 @@ class HomeScreen extends StatelessWidget {
               CardSwiper(movies: moviesProvider.onDisplayMovie),
               //Listado de peliculas
               MovieSlider(
-                  movies: moviesProvider.popularMovies, title: "Populares!")
+                movies: moviesProvider.popularMovies,
+                title: "Populares!",
+                onNextPage: () => moviesProvider.getPopularMovies(),
+              )
             ],
           ),
         ));
